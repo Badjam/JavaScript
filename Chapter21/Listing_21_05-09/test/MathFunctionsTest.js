0018ce9d0568c489d460b63f28b52dbd645fd5d2
Philip Ackermann <info@philipackermann.de>
2022-02-01 14:39:37 +0100

    Update QUnit examples into English

--- Chapter21/Listing_21_05-09/test/MathFunctionsTest.js
+++ Chapter21/Listing_21_05-09/test/MathFunctionsTest.js
@@ -1,42 +1,42 @@
 'use strict';
-QUnit.test('MathFunctions.compare() soll 1 zurückgeben, wenn die erste Zahl größer ist.', (assert) => {
+QUnit.test('MathFunctions.compare() should return 1, if the first number is greater.', (assert) => {
   const number1 = 7;
   const number2 = 5;
   const result = MathFunctions.compare(number1, number2);
   assert.equal(
-    result,               // Tatsächliches Ergebnis
-    1,                    // Erwartetes Ergebnis
-    '7 ist größer als 5'  // Zusätzliche Meldung
+    result,                // Actual result
+    1,                     // Expected result
+    '7 is greater than 5'  // Additional message
   );
 });
-QUnit.test('MathFunctions.compare() soll -1 zurückgeben, wenn die erste Zahl kleiner ist.', (assert) => {
+QUnit.test('MathFunctions.compare() should return -1, if the first number is smaller.', (assert) => {
   const number1 = 5;
   const number2 = 7;
   const result = MathFunctions.compare(number1, number2);
   assert.equal(
-    result,               // Tatsächliches Ergebnis
-    -1,                   // Erwartetes Ergebnis
-    '5 ist kleiner als 7' // Zusätzliche Meldung
+    result,                // Actual result
+    -1,                    // Expected result
+    '5 is smaller than 7'  // Additional message
   );
 });
-QUnit.test('MathFunctions.compare() soll 0 zurückgeben, wenn beide Zahlen gleich sind.', (assert) => {
+QUnit.test('MathFunctions.compare() should return 0, if both numbers are equal.', (assert) => {
   const number1 = 5;
   const number2 = 5;
   const result = MathFunctions.compare(number1, number2);
   assert.equal(
-    result,               // Tatsächliches Ergebnis
-    0,                    // Erwartetes Ergebnis
-    '5 ist gleich 5'      // Zusätzliche Meldung
+    result,           // Actual result
+    0,                // Expected result
+    '5 equals 5'      // Additional message
   );
 });
-QUnit.test('MathFunctions.compare() soll einen Fehler werfen, wenn keine Zahlen übergeben wurden.', (assert) => {
+QUnit.test('MathFunctions.compare() should throw an error, if no numbers are passed.', (assert) => {
   const number1 = "5";
   const number2 = "5";
   assert.throws(
     () => {
       MathFunctions.compare(number1, number2)
     },
-    new TypeError('Nur Zahlen erlaubt.'),
-    'Zeichenketten werden nicht akzeptiert.'
+    new TypeError('Only numbers are allowed.'),
+    'Strings are not accepted.'
   );
 });
